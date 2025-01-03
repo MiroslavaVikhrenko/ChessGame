366b3b3eb090fbf74fd2131ed7661f77ca950c31
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-03 16:13:27 -0700

    Add board initialization logic into code behind, now we have empty images for cells

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -8,17 +8,39 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLogic;
 
 namespace ChessUI
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml (code behind)
     /// </summary>
     public partial class MainWindow : Window
     {
+        //create a 2d array for the image controls
+        //it must have 8 rows and 8 columns to match the board
+        private readonly Image[,] pieceImages = new Image[8, 8];
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBoard();
+        }
+
+        private void InitializeBoard()
+        {
+            for (int r = 0; r < 8; r++)
+            {
+                for (int c = 0; c < 8; c++)
+                {
+                    //for each position create an image control
+                    Image image = new Image();
+                    //store it in 2d array
+                    pieceImages[r, c] = image;
+                    //add it in the UniformGrid as a child
+                    PieceGrid.Children.Add(image);
+                }
+            }
+            //we will call InitializeBoard() from the constructor after InitializeComponent()
         }
     }
 }
