070bd3b4d55ae632b27833113d7e97c6b0c9e678
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-03 17:49:01 -0700

    Add a method MovePositionsInDir() to Piece base class which returns all reachable positions in a given direction (bishop, rook, queen)

--- ChessLogic/Pieces/Piece.cs
+++ ChessLogic/Pieces/Piece.cs
@@ -16,5 +16,38 @@ namespace ChessLogic
         //the method returns a collection containing all the moves it can make
         //the position parameter is required because the pieces don't store their own position on the board       
         public abstract IEnumerable<Move> GetMoves(Position from, Board board);
+        //3 of the pieces move using the similar pattern (bishop, rook, queen)
+        //These pieces can all move as many positions as they want in certain directions
+        //Helper method which returns all reachable positions in a given direction
+        protected IEnumerable<Position> MovePositionsInDir(Position from, Board board, Direction dir)
+        {
+            //loop starts at the position "from" plus one step in the given direction => Position pos = from + dir
+            //it continues while the position is inside the board => Board.IsInside(pos)
+            //and after each iteration it takes another step => pos += dir
+            //it's easy to do because of overloaded "+" operator
+            for (Position pos = from + dir; Board.IsInside(pos); pos += dir)
+            {
+                //check if the current position is empty
+                if (board.IsEmpty(pos))
+                {
+                    //if so, it is reachable and we can yield return it
+                    yield return pos;
+                    //and continue to the next position immediately
+                    continue;
+                }
+                //otherwise there IS a piece at the position
+                Piece piece = board[pos];
+                //if it belongs to the opponent 
+                if (piece.Color != Color)
+                {
+                    //then it can be captured, so its position is also reachable 
+                    yield return pos;
+                }
+                //if the piece does not belong to the opponent => its position is NOT reachable 
+                //but either way we encountered a piece and since the bishop, rook, queen canNOT jump =>
+                //we don't have to check any more positions
+                yield break;
+            }
+        }
     }
 }
