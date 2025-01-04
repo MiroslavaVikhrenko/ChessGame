1bf03d2134f8940b95cecf51647d2816a52c710c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-03 17:02:45 -0700

    Create a MoveType enum for normal and special types of move

---
+++ ChessLogic/MoveType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    //contain a value for each type of move in the game (normal + special moves)
+    //Рокировка - Castling
+    public enum MoveType
+    {
+        Normal,
+        CastleKS,
+        CastleQS,
+        DoublePawn,
+        EnPassant,
+        PawnPromotion
+    }
+}
