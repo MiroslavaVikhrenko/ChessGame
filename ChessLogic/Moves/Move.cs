72a396b4cc0293c82190bb1bb2f4ca796f34d921
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2025-01-03 17:17:37 -0700

    Create a NormalMove class

--- ChessLogic/Moves/Move.cs
+++ ChessLogic/Moves/Move.cs
@@ -11,9 +11,9 @@ namespace ChessLogic
     {
         public abstract MoveType Type { get; }
         //"From" position = this is where the piece moves from
-        public abstract Position fromPos { get; }
+        public abstract Position FromPos { get; }
         //"To" position = this is where the piece moves to
-        public abstract Position toPos { get; }
+        public abstract Position ToPos { get; }
         //a bit like the command design pattern
         public abstract void Execute(Board board);
     }
---
+++ ChessLogic/Moves/NormalMove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessLogic
+{
+    //this class will be used for all moves that simply move the piece from one position to another
+    //the move may capture an opponent piece but otherwise it won't have any side effects
+    public class NormalMove : Move
+    {
+        public override MoveType Type => MoveType.Normal;
+        public override Position FromPos { get; }
+        public override Position ToPos { get; }
+        public NormalMove(Position from, Position to)
+        {
+            FromPos = from;
+            ToPos = to;
+        }
+    }
+}
